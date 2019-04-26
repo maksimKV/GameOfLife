dfa8fa5249098e57f2db5287443527064db5cae2
Maksim Kanev <maks_kv@yahoo.com>
2019-04-26 22:50:31 +0300

    Initial commit

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,26 @@
+﻿using GameSimulation;
+
+namespace GameOfLife
+{
+    public class Program
+    {
+        // Rules of the game.
+        private const int Heigth = 10;
+        private const int Width = 30;
+        private const uint MaxRuns = 100;
+
+        private static void Main(string[] args)
+        {
+            int runs = 0;
+            GameProcess game = new GameProcess(Heigth, Width);
+
+            while (runs++ < MaxRuns)
+            {
+                game.DrawAndGrow();
+
+                // Slowing down the game, so that the results are readable.
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ GameSimulation/GameProcess.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace GameSimulation
+{
+    public class GameProcess
+    {
+        private int Heigth;
+        private int Width;
+        private bool[,] cells;
+
+        /// <summary>
+        /// Initializes a new game.
+        /// </summary>
+        /// <param name="Heigth">Heigth of the cell field.</param>
+        /// <param name="Width">Width of the cell field.</param>
+        public GameProcess(int Heigth, int Width)
+        {
+            this.Heigth = Heigth;
+            this.Width = Width;
+            cells = new bool[Heigth, Width];
+            GenerateField();
+        }
+
+        /// <summary>
+        /// Advances the game to the next generation and prints the current state to console.
+        /// </summary>
+        public void DrawAndGrow()
+        {
+            DrawGame();
+            Grow();
+        }
+
+        /// <summary>
+        /// Advances the game to the next generation according to GoL's ruleset.
+        /// </summary>
+        private void Grow()
+        {
+            for (int i = 0; i < Heigth; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    int numOfAliveNeighbors = GetNeighbors(i, j);
+
+                    if (cells[i, j])
+                    {
+                        if (numOfAliveNeighbors < 2)
+                        {
+                            cells[i, j] = false;
+                        }
+
+                        if (numOfAliveNeighbors > 3)
+                        {
+                            cells[i, j] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (numOfAliveNeighbors == 3)
+                        {
+                            cells[i, j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks how many alive neighbours surround the cell.
+        /// </summary>
+        /// <param name="x">X-coordinate of the cell.</param>
+        /// <param name="y">Y-coordinate of the cell.</param>
+        /// <returns>The number of alive neighbours.</returns>
+        private int GetNeighbors(int x, int y)
+        {
+            int NumOfAliveNeighbors = 0;
+
+            for (int i = x - 1; i < x + 2; i++)
+            {
+                for (int j = y - 1; j < y + 2; j++)
+                {
+                    if (!((i < 0 || j < 0) || (i >= Heigth || j >= Width)))
+                    {
+                        if (cells[i, j] == true) NumOfAliveNeighbors++;
+                    }
+                }
+            }
+            return NumOfAliveNeighbors;
+        }
+
+        /// <summary>
+        /// Draws the game to the console.
+        /// </summary>
+        private void DrawGame()
+        {
+            for (int i = 0; i < Heigth; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write(cells[i, j] ? "x" : " ");
+                    if (j == Width - 1) Console.WriteLine("\r");
+                }
+            }
+            Console.SetCursorPosition(0, Console.WindowTop);
+        }
+
+        /// <summary>
+        /// Initializes the field with random boolean values.
+        /// </summary>
+        private void GenerateField()
+        {
+            Random generator = new Random();
+            int number;
+            for (int i = 0; i < Heigth; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    number = generator.Next(2);
+                    cells[i, j] = ((number == 0) ? false : true);
+                }
+            }
+        }
+    }
+}
